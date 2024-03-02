69b61dbe8e6d3bdcc58f39975b563367e704c138
CodeNinjasDripping <cndsstudio@gmail.com>
2024-03-02 13:00:01 -0600

    init commit

---
+++ My project/Assets/Scripts/MovingAndRotatingObjects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingAndRotatingObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 50f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+}
