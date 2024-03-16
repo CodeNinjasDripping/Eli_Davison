1049fcc1d33f4311b782573880ee90a89921c8b4
CodeNinjasDripping <cndsstudio101@gmail.com>
2024-03-16 14:14:28 -0500

    changed branches

---
+++ My project/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] shapePrefabs;
+    public float spawnDelay = 2f;
+    public float spawnTime = 3f;
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+
+    public void GameOver()
+    {
+        CancelInvoke("Spawn");
+    }
+}
